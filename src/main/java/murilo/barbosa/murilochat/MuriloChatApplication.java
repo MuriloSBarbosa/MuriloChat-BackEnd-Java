1af20c4721e24ea7694d175a071ef3e1abba66c4
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-09-12 19:13:08 -0300

    Initial commit

---
+++ src/main/java/murilo/barbosa/murilochat/MuriloChatApplication.java
@@ -0,0 +1,13 @@
+package murilo.barbosa.murilochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuriloChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuriloChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/murilo/barbosa/murilochat/MuriloChatApplicationTests.java
@@ -0,0 +1,13 @@
+package murilo.barbosa.murilochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuriloChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
